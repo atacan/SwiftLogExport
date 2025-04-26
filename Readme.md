1c0f84cb49ac2942710b5c352cacd5a7257995ff
atacan <atacandur@icloud.com>
2025-04-26 18:48:01 +0200

    Add Readme and enhance LogRecord protocol with label property
    - Introduced a new Readme.md file acknowledging the swift-otel project.
    - Updated the LogRecord protocol to include a label property for better log identification.
    - Modified DefaultLogRecord to implement the new label property and adjusted the initializer accordingly.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Acknowledgements
+
+- [swift-otel](https://github.com/swift-otel/swift-otel): The code is mostly copied from the swift-otel project.
--- Sources/SwiftLogExport/LogRecord.swift
+++ Sources/SwiftLogExport/LogRecord.swift
@@ -1,23 +1,27 @@
 import Logging
 
 public protocol LogRecord: Equatable, Sendable {
+    var label: String { get }
     var message: Logger.Message { get }
     var level: Logger.Level { get }
     var metadata: Logger.Metadata { get }
 }
 
 public struct DefaultLogRecord: LogRecord, Equatable, Sendable {
+    public var label: String
     public var message: Logger.Message
     public var level: Logger.Level
     public var metadata: Logger.Metadata
     public var timeNanosecondsSinceEpoch: UInt64
 
-    package init(
+    public init(
+        label: String,
         message: Logger.Message,
         level: Logger.Level,
         metadata: Logger.Metadata,
         timeNanosecondsSinceEpoch: UInt64
     ) {
+        self.label = label
         self.message = message
         self.level = level
         self.metadata = metadata
