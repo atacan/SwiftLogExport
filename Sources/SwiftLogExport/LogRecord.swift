b1f71164154ff90d17ba72bd624aa41423b300e4
atacan <atacandur@icloud.com>
2025-04-28 22:20:10 +0200

    Refactor LogRecord and introduce LoggingHandler for enhanced logging capabilities
    - Simplified the LogRecord protocol by removing unnecessary properties and adjusting the initializer to include source, file, function, line, and timestamp.
    - Added a new LoggingHandler struct that implements the LogHandler protocol, facilitating the creation and processing of log records with dynamic metadata handling and batch processing support.

--- Sources/SwiftLogExport/LogRecord.swift
+++ Sources/SwiftLogExport/LogRecord.swift
@@ -1,43 +1,16 @@
-//===----------------------------------------------------------------------===//
-//
-// The code is mostly taken from the Swift OTel project
-//
-// Copyright (c) 2024 the Swift OTel project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE.txt for license information
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-//===----------------------------------------------------------------------===//
-
+import Foundation
 import Logging
 
 public protocol LogRecord: Equatable, Sendable {
-    var label: String { get }
-    var message: Logger.Message { get }
-    var level: Logger.Level { get }
-    var metadata: Logger.Metadata { get }
-}
-
-public struct DefaultLogRecord: LogRecord, Equatable, Sendable {
-    public var label: String
-    public var message: Logger.Message
-    public var level: Logger.Level
-    public var metadata: Logger.Metadata
-    public var timeNanosecondsSinceEpoch: UInt64
-
-    public init(
+    init(
         label: String,
         message: Logger.Message,
         level: Logger.Level,
         metadata: Logger.Metadata,
-        timeNanosecondsSinceEpoch: UInt64
-    ) {
-        self.label = label
-        self.message = message
-        self.level = level
-        self.metadata = metadata
-        self.timeNanosecondsSinceEpoch = timeNanosecondsSinceEpoch
-    }
+        source: String,
+        file: String,
+        function: String,
+        line: UInt,
+        timestamp: Date
+    )
 }
